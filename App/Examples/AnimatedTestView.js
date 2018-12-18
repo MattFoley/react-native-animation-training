22303bec23519031ca43e08b6c06c88fe44cc9a9
Tj Fallon <tfallon@mail.depaul.edu>
2018-12-17 22:53:55 -0800

    Animated example.

--- App/Examples/AnimatedTestView.js
+++ App/Examples/AnimatedTestView.js
@@ -7,27 +7,48 @@ import {
   Text,
   SafeAreaView,
   TouchableOpacity,
+  Animated,
+  Easing
 } from 'react-native';
 
 import styles from './styles';
 
 export default class AnimatedTestView extends Component {
 
-  animateMe = () => {
+  constructor () {
+    super()
+    this.spinValue = new Animated.Value(0)
+  }
 
+  animateMe = () => {
+    this.spinValue.setValue(0)
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 1,
+        duration: 400,
+        easing: Easing.linear
+      }
+    ).start()
   }
 
   render() {
+    
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    })
+
     return (
       <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <View style={styles.container}>
           <TouchableOpacity style={[styles.square, { backgroundColor: 'white' }]}
                             onPress={this.animateMe}>
-            <View style={styles.square}>
+            <Animated.View style={[styles.square, {transform: [{rotate: spin}] }]}>
               <Text style={styles.squareText}>
                 {'Tap Me'}
               </Text>
-            </View>
+            </Animated.View>
           </TouchableOpacity>
         </View>
       </SafeAreaView>
