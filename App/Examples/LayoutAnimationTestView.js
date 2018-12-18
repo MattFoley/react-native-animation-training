a7a5edfe671d42928348d94591b24dbb975cd0a9
Tj Fallon <tfallon@mail.depaul.edu>
2018-12-17 23:12:14 -0800

    Layout animation example

--- App/Examples/LayoutAnimationTestView.js
+++ App/Examples/LayoutAnimationTestView.js
@@ -7,23 +7,33 @@ import {
   Text,
   SafeAreaView,
   TouchableOpacity,
+  LayoutAnimation,
 } from 'react-native';
 
 import styles from './styles';
 
-export default class AnimatableTestView extends Component {
+export default class LayoutAnimationTestView extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
   animateMe = () => {
-
+    LayoutAnimation.spring();
+    this.setState({
+      shouldTransform: !this.state.shouldTransform,
+    });
   }
 
   render() {
+    const transform = this.state.shouldTransform ? {layout: 'absolute', top: -50} : {};
+
     return (
       <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <View style={styles.container}>
           <TouchableOpacity style={[styles.square, { backgroundColor: 'white' }]}
                             onPress={this.animateMe}>
-            <View style={styles.square}>
+            <View style={[styles.square, transform]}>
               <Text style={styles.squareText}>
                 {'Tap Me'}
               </Text>
